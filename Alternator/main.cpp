1c26643124b07d400771576fa671e0aaddd25fb5
panassevich <panassevichm@gmail.com>
2022-04-24 19:27:12 +0300

    Переписал метод scnfSearch, предположительно исправил гигантское потребление памяти и ошибку

--- Alternator/main.cpp
+++ Alternator/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
+#include <sstream>
 
 using namespace std;
+
 struct node
 {
     struct node* left = nullptr;
@@ -265,84 +268,180 @@ void vecClean(vector<vector<string>>& vec)
     vec.swap(ans);
 }
 
-vector<vector<string>> sknfSearch(node* node, int wantedValue, vector<vector<string>> vec) {
+//vector<vector<string>> sknfSearch(node* node, int wantedValue, vector<vector<string>> vec) {
+//
+//    //если попалась переменная
+//    if (node->value[0]=='a') {
+//
+//        //если вектор пустой
+//        if (!(vec.empty()) && vec[0][0]=="n") {
+//            vector <string> temp;
+//            if (wantedValue == 1) {
+//                temp.push_back(node->value);
+//            }
+//            else {
+//                string s = "A";
+//                for (int i = 1; i < node->value.size(); i++) {
+//                    s.push_back(node->value[i]);
+//                }
+//                temp.push_back(s);
+//            }
+//            vec.push_back(temp);
+//            return vec;
+//        }
+//        else {
+//            vecClean(vec);
+//            string valueToPost;
+//            if (wantedValue == 1) {
+//                valueToPost = node->value;
+//            }
+//            else {
+//                valueToPost = "A";
+//                for (int i = 1; i < node->value.size(); i++) {
+//                    valueToPost.push_back(node->value[i]);
+//                }
+//            }
+//
+//            for (int i = 0; i < vec.size();i++) {
+//                if (vec[i].empty()) {
+//                    continue;
+//                }
+//                bool isPermissible = true;
+//                for (int j = 0; j < vec[i].size(); j++) {
+//                    if (vec[i][j] == valueToPost) {
+//                        //такая переменная уже есть в данной комбинации, выходим.
+//                        isPermissible = false;
+//                        break;
+//                    }
+//                    else if (vec[i][j].substr(1) == valueToPost.substr(1)) {
+//                        if (vec[i][j][0] - valueToPost[0] != 0) {
+//                            isPermissible = false;
+//                            vector <string> x;
+//                            vec[i].clear();
+//                            vec[i].swap(x);
+//                            break;
+//                        }
+//                    }
+//                }
+//                if (isPermissible) {
+//                    vec[i].push_back(valueToPost);
+//                }
+//            }
+//            vecClean(vec);
+//            return vec;
+//        }
+//    }
+//
+//    vector<pair<int, int>> pairs = getSuitableOperands(node->value[0], wantedValue);
+//    vector<vector<string>> answer;
+//    if (node->value[0] == '!') {
+//        answer = sknfSearch(node->right, pairs[0].second, vec);
+//        return answer;
+//    }
+//    for (int i = 0; i < pairs.size(); i++) {
+//        vector<vector<string>> res = sknfSearch(node->left, pairs[i].first, vec);
+//        res = sknfSearch(node->right, pairs[i].second, res);
+//        //vecClean(res);
+//        answer.insert(answer.end(), res.begin(), res.end());
+//    }
+//    //vecClean(answer);
+//    return answer;
+//}
 
-    //если попалась переменная
-    if (node->value[0]=='a') {
+bool isOperator(char symbol) {
+    int arraySize = sizeof(priorityArray)/sizeof(priorityArray[0]);
 
-        //если вектор пустой
-        if (vec.empty()) {
-            vector <string> temp;
-            if (wantedValue == 1) {
-                temp.push_back(node->value);
-            }
-            else {
-                string s = "A";
-                for (int i = 1; i < node->value.size(); i++) {
-                    s.push_back(node->value[i]);
-                }
-                temp.push_back(s);
-            }
-            vec.push_back(temp);
-            return vec;
+    for (int i = 0; i < arraySize; i++) {
+        if (symbol == priorityArray[i])
+            return true;
+    }
+    return false;
+}
+
+vector<vector<string>> sknfSearch(int wantedValue, vector<vector<string>> vec, node* node) {
+    
+    if (isOperator(node->value[0])) {
+        //если попали в оператор
+        vector<pair<int, int>> pairs = getSuitableOperands(node->value[0], wantedValue);
+        
+        vector<vector<string>> answer;
+
+        if (node->value[0] == '!') {
+            answer = sknfSearch(pairs[0].second, vec, node->right);
         }
         else {
-            //vecClean(vec);
-            string valueToPost;
-            if (wantedValue == 1) {
-                valueToPost = node->value;
+            for (int i = 0; i < pairs.size(); ++i) {
+                vector<vector<string>> curResult = sknfSearch(pairs[i].second, sknfSearch(pairs[i].first, vec, node->left), node->right);
+                answer.insert(answer.end(), curResult.begin(), curResult.end());
             }
-            else {
-                valueToPost = "A";
-                for (int i = 1; i < node->value.size(); i++) {
-                    valueToPost.push_back(node->value[i]);
-                }
-            }
+        }
+        return answer;
 
-            for (int i = 0; i < vec.size();i++) {
-                if (vec[i].empty()) {
+    }
+    else {
+        //если попали в переменную
+        string valueToPost = node->value;
+
+        if (wantedValue == 0)
+            valueToPost[0] = 'A';
+
+        //если вектор пустой, просто добавим туда одну комбинацию из одной переменной
+        if (vec.size() == 0) {
+            vector<string> tempVec;
+
+            tempVec.push_back(valueToPost);
+            vec.push_back(tempVec);
+
+            return vec;
+
+        }
+        else {
+            //внешний цикл по комбинациям
+            stringstream curVarNumberContainer(node->value.substr(1, node->value.size() - 1));
+            int curVarNumber;
+            curVarNumberContainer >> curVarNumber;
+
+            for (int i = 0; i < vec.size(); ++i) {
+                if (vec[i].size() == 0)
                     continue;
-                }
-                bool isPermissible = true;
-                for (int j = 0; j < vec[i].size(); j++) {
-                    if (vec[i][j] == valueToPost) {
-                        //такая переменная уже есть в данной комбинации, выходим.
-                        isPermissible = false;
+                //внутренний цикл по вхождениям переменной в комбинацию
+                bool needToPost = true;
+                for (int j = 0; j < vec[i].size(); ++j) {
+                    stringstream varInVecContainer(vec[i][j].substr(1, vec[i][j].size() - 1));
+                    int varInVec;
+                    varInVecContainer >> varInVec;
+
+                    //если нашли переменную с номером больше текущей, вставляем сюда.
+                    if (curVarNumber < varInVec) {
+                        vec[i].insert(vec[i].begin() + j, valueToPost);
+                        needToPost = false;
                         break;
                     }
-                    else if (vec[i][j].substr(1) == valueToPost.substr(1)) {
-                        if (vec[i][j][0] - valueToPost[0] != 0) {
-                            isPermissible = false;
-                            vector <string> x;
-                            //vec[i].clear();
-                            vec[i].swap(x);
-                            break;
+                    //если наткнулись на эту же переменную, нужно проверить, в каком виде она входит
+                    else if (curVarNumber == varInVec) {
+                        
+                        //если она уже входит с другим знаком - комбинация дефектная, в мусор
+                        if (valueToPost[0] != vec[i][j][0]) {
+                            vec[i].clear(); 
                         }
+                        needToPost = false;
+                        break;
                     }
                 }
-                if (isPermissible) {
+                if (needToPost)
                     vec[i].push_back(valueToPost);
-                }
+            }
+
+            //очищаем вектор от всех пустых комбинаций.(освобождаем очень много памяти)
+            vecClean(vec);
+            //если вектор после очистки стал пустым, добавим один пустой элемент. Это нужно для разграничения пустых изначально и "опустошённых" векторов.
+            if (vec.empty()) {
+                vector<string> temp;
+                vec.push_back(temp);
             }
             return vec;
         }
     }
-
-    vector<pair<int, int>> pairs = getSuitableOperands(node->value[0], wantedValue);
-    vector<vector<string>> answer;
-    if (node->value[0] == '!') {
-        answer = sknfSearch(node->right, pairs[0].second, vec);
-        return answer;
-    }
-    for (int i = 0; i < pairs.size(); i++) {
-        vector<vector<string>> res = sknfSearch(node->left, pairs[i].first, vec);
-        //vecClean(res);
-        res = sknfSearch(node->right, pairs[i].second, res);
-        vecClean(res);
-        answer.insert(answer.end(), res.begin(), res.end());
-    }
-    //vecClean(answer);
-    return answer;
 }
 
 
@@ -350,34 +449,27 @@ vector<vector<string>> sknfSearch(node* node, int wantedValue, vector<vector<str
 int main()
 {
     vector<vector<string>> stash;
+    //vector<string> buf{"n"};
+    //stash.push_back(buf);
     node* root = new node;
     string calculate;
     cin >> calculate;
     addnode(calculate, root);
-    inOrderTravers(root);
+    //inOrderTravers(root);
     cout << endl;
-    stash = sknfSearch(root, 0, stash);
 
-    /*for (int i = 0; i < stash.size(); i++) 
-            for (int j = 0; j < stash[i].size(); j++) {
-                if (isupper(stash[i][j])) {
-                    cout << '!' << (char)tolower(stash[i][j]) << " ";
-                }
-                else {
-                    cout << stash[i][j] << " ";
-                }
-            }
-            cout << endl;
-    }*/
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+     stash = sknfSearch(0, stash, root);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[ms]" << std::endl;
     for (int i = 0; i < stash.size(); i++) {
-        
             for (int j = 0; j < stash[i].size(); j++) {
                 cout << stash[i][j] << " ";
             }
             cout << endl;
         
     }
-     return 0;
+    return 0;
 }
 
 
